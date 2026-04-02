c66554056c7a422e48c643df41bb892423086be0
Max Zinovik <zinovik@gmail.com>
2026-04-02 20:06:28 +0200

    initial commit

---
+++ src/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
