6e91131390f0a0c1cd80ab24a56f55676d9d519f
Max Zinovik <zinovik@gmail.com>
2026-04-17 06:05:17 +0200

    remove isDevMode for schedulers

--- spacelift/schedulers.tf
+++ spacelift/schedulers.tf
@@ -30,6 +30,40 @@ resource "google_cloud_scheduler_job" "bgg_top1000_bot_dev" {
   }
 }
 
+resource "google_cloud_scheduler_job" "bgg_top100_bot" {
+  name     = "bgg-top100-bot"
+  project  = var.project_id
+  schedule = "0 9 * * 6"
+
+  http_target {
+    # uri         = "https://europe-central2-boardgamegeek-bots.cloudfunctions.net/bgg-top100-bot?channelId=@bggtop100&isDevMode=off"
+    uri         = "https://europe-central2-boardgamegeek-bots.cloudfunctions.net/bgg-top100-bot?channelId=446618160&isDevMode=off"
+    http_method = "GET"
+
+    oidc_token {
+      service_account_email = "boardgamegeek-bots@appspot.gserviceaccount.com"
+      audience              = "https://europe-central2-boardgamegeek-bots.cloudfunctions.net/bgg-top100-bot"
+    }
+  }
+}
+
+resource "google_cloud_scheduler_job" "bgg_top1000_bot" {
+  name     = "bgg-top1000-bot"
+  project  = var.project_id
+  schedule = "0 9 * * 6"
+
+  http_target {
+    # uri         = "https://europe-central2-boardgamegeek-bots.cloudfunctions.net/bgg-top1000-bot?channelId=@bggtop1000&isDevMode=off"
+    uri         = "https://europe-central2-boardgamegeek-bots.cloudfunctions.net/bgg-top1000-bot?channelId=446618160&isDevMode=off"
+    http_method = "GET"
+
+    oidc_token {
+      service_account_email = "boardgamegeek-bots@appspot.gserviceaccount.com"
+      audience              = "https://europe-central2-boardgamegeek-bots.cloudfunctions.net/bgg-top1000-bot"
+    }
+  }
+}
+
 resource "google_cloud_scheduler_job" "bgg_games_ranks_cacher" {
   name     = "bgg-games-ranks-cacher"
   project  = var.project_id
